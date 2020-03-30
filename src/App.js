93f4ef7201fedb941b50dc582cb8561a8ef91b13
Michielvst <michiel--vs@outlook.com>
2020-03-30 15:20:05 +0200

    colorPicker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ColorPicker from "./components/ColorPicker";
+import Game from "./components/Game";
 
 function App() {
+  const [player1Color, setPlayer1Color] = useState("");
+  const [player2Color, setPlayer2Color] = useState("");
+  const [showColorPicker, setShowColorPicker] = useState(true);
+  const [showGame, setShowGame] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showColorPicker === true && (
+        <ColorPicker
+          player1Color={player1Color}
+          setPlayer1Color={setPlayer1Color}
+          player2Color={player2Color}
+          setPlayer2Color={setPlayer2Color}
+          setShowGame={setShowGame}
+        />
+      )}
+      {showGame === true && <Game />}
     </div>
   );
 }
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "pink",
+  "yellow",
+  "purple",
+  "grey",
+  "orange"
+];
+
+export default function ColorPicker(props) {
+  const [selectedPlayer, setSelectedPlayer] = useState("Player 1");
+  const [hideStartButton, setHideStartButton] = useState(true);
+
+  function handleColorClick(e) {
+    if (hideStartButton === false) {
+      return;
+    }
+    if (selectedPlayer === "Player 1") {
+      props.setPlayer1Color(e.currentTarget.id);
+      setSelectedPlayer("Player 2");
+    } else {
+      props.setPlayer2Color(e.currentTarget.id);
+      setHideStartButton(false);
+      setSelectedPlayer("");
+    }
+  }
+
+  function checkIfAllreadyClicked(color) {
+    if (props.player2Color !== "") {
+      return "unClickable";
+    } else if (color === props.player1Color) {
+      return "unClickable";
+    } else {
+      return "clickable";
+    }
+  }
+
+  return (
+    <div className="colorPicker">
+      <header className="playersColorSelect">
+        <h1
+          style={{ color: props.player1Color }}
+          className={selectedPlayer === "Player 1" ? "pulsing" : ""}
+        >
+          Player 1
+        </h1>
+        <p hidden={!hideStartButton}>{selectedPlayer} select a color!</p>
+        <button hidden={hideStartButton}>Start</button>
+        <h1
+          style={{ color: props.player2Color }}
+          className={selectedPlayer === "Player 2" ? "pulsing" : ""}
+        >
+          Player 2
+        </h1>
+      </header>
+      <div className="colorDiv">
+        {colors.map(color => {
+          return (
+            <div
+              onClick={handleColorClick}
+              className={`color ${checkIfAllreadyClicked(color)}`}
+              style={{ backgroundColor: color }}
+              key={color}
+              id={color}
+            >
+              test
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
